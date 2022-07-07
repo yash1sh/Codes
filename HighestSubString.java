3fe65022e7a3993b0453fbe546d2edb30979adc3
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-07-07 10:48:42 +0530

    Add files via upload
    Consider a string we have to find the highest substring where the char are not repeated

---
+++ HighestSubString.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class HighestSubString{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str=sc.next();
+        HashMap <Character,Integer> hmap=new HashMap<>();
+        int left=0,length=0;
+        if(str.length()<=1)
+            System.out.println("1");
+        for(int right=0;right<str.length();right++)
+        {
+            if(hmap.containsKey(str.charAt(right)))
+            {
+                left=right;
+                hmap.clear();
+            }
+            else{
+                hmap.put(str.charAt(right),right);
+            }
+            length=Math.max(length, right-left+1);
+            
+        }
+        System.out.println(length);
+
+
+
+    }
+}
