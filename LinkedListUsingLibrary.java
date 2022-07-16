d6391a566210504ccd5948d91acfef23e3669b70
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-07-16 16:04:20 +0530

    Add files via upload
    Linked list reverse problem

---
+++ LinkedListUsingLibrary.java
@@ -0,0 +1,80 @@
+import java.util.*;
+public class LinkedListUsingLibrary {
+    public static void main(String[] args) {
+        int ch,value;
+        boolean exit=true;
+        List<Integer> list=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        while(exit)
+        {
+            System.out.println("Enter the number operation you want to do \n1) add \n2)remove \n3) Contains \n4)Reverse \n5)print 6)clear \n7)exit ");
+            ch=sc.nextInt();
+            switch(ch)
+            {
+                case 1: System.out.println("Enter the value integer only");
+                    value=sc.nextInt();
+                    list.add(value);
+                    break;
+                case 2:
+                    if(list.isEmpty())
+                    {
+                        System.out.println("List is empty");
+                    }
+                    else{ 
+                        System.out.println("select choice 1)index 2) value");
+                        value=sc.nextInt();
+                        if(value==1)
+                        {
+                            System.out.println("Enter the index val");
+                            value=sc.nextInt();
+                            list.remove(value);
+                        }
+                        else{
+                            System.out.println("Enter the value");
+                            value=sc.nextInt();
+                            list.remove(Integer.valueOf(value));
+                        }
+                        System.out.println("Removed....");
+                    }
+                    break;
+                case 3:System.out.println("Enter the value to check if it contains");
+                        value=sc.nextInt();
+                        if(list.contains(value))
+                            System.out.println(value +" contains");
+                        else
+                            System.out.println(value+" does not contains");
+                        break;
+                        case 4: int first=0;
+                        int last=list.size()-1;
+                        
+                        while(first<last)
+                        {
+                            value=list.get(last);
+                
+                            list.set(last, list.get(first));
+
+                            list.set(first, value);
+
+                            first++;
+                            last--;
+                        }
+                case 5:if(list.isEmpty())   System.out.println("List is empty");
+                        else{
+                        System.out.print("List values are : ");
+                        for(int i=0;i<list.size();i++)
+                            System.out.print( list.get(i)+"\t");;
+                        System.out.println();
+                        }
+                        break;
+                case 6: list.clear();
+                        System.out.println("List is been cleared ");
+                        break;
+                case 7:exit=false;
+                            break;
+                
+                default:System.out.println("Invalid input");
+            }
+        } 
+        sc.close();     
+    }
+}
---
+++ linkedList.java
@@ -0,0 +1,135 @@
+
+class listNode{
+    Object name;
+    listNode nextNode;
+
+    listNode(Object obj){
+        this(obj,null);
+    }
+    listNode(Object obj,listNode nextNode)
+    {
+        this.name=obj;
+        this.nextNode=nextNode;
+    }
+    Object getObject()
+    {
+        return name;
+    }
+
+    listNode get_nextNode()
+    {
+        return nextNode;
+    }
+}
+
+class list{
+    private listNode firstNode;
+    private listNode lastNode;
+    private String name;
+
+    public list()
+    {
+        this("list");
+    }
+    public list(String name)
+    {
+        this.name=name;
+        firstNode=lastNode=null;
+    }
+    public boolean isEmpty()
+    {
+        return firstNode==null;
+    }
+    public void insertAtFront(Object item)
+    {
+        if(isEmpty())
+            firstNode=lastNode=new listNode(item);
+        else
+            firstNode=new listNode(item,firstNode);
+    }
+    public void insertAtBack(Object item)
+    {
+        if(isEmpty())
+            firstNode=lastNode=new listNode(item);
+        else
+            lastNode=lastNode.nextNode=new listNode(item);
+    }
+    public Object removeAtFront() 
+    {
+        if(isEmpty())
+            System.out.println(this.name+": doest has no values");
+
+            Object removeObject=firstNode.name;
+        
+            if(firstNode==lastNode)
+                firstNode=lastNode=null;
+            else
+                firstNode=firstNode.nextNode;
+        
+        return removeObject;
+    }
+
+    public Object removeAtBack() 
+    {
+        if(isEmpty())
+            System.out.println(this.name+": doest has no values");
+
+            Object removeObject=lastNode.name;
+        
+            if(firstNode==lastNode)
+                firstNode=lastNode=null;
+            else{
+                listNode Current=firstNode;
+                while(Current.nextNode!=lastNode)
+                    Current=Current.nextNode;
+                lastNode=Current;
+                Current.nextNode=null;
+            }
+        
+        return removeObject;
+    }
+    
+    public void print()
+    {
+        if(isEmpty())
+        {
+            System.out.println(
+                "List is empty"
+            );
+        }
+        else{
+            System.out.print("The "+name+" is:");
+            listNode Current=firstNode;
+            while(Current!=null)
+            {
+                System.out.print(Current.name);
+                Current=Current.nextNode;
+            }
+        }
+        System.out.println();
+    }
+}
+
+
+public class linkedList {
+
+
+    public static void main(String[] args) {
+        list list=new list();
+        list.insertAtFront(1);
+        list.insertAtFront(2);
+        list.insertAtBack(9);
+        list.insertAtFront(3);
+        list.insertAtFront(4);
+        list.insertAtFront(5);
+        
+        Object removeObject=list.removeAtFront();
+        System.out.println("The removed object from front is "+removeObject);
+
+        removeObject=list.removeAtBack();
+        System.out.println("The removed object from back is "+removeObject);
+
+        list.print();
+
+    }
+}
